933058536952bb38153ec3343db6cb79ffc7b971
Apostolique <jeandavidmoisan@gmail.com>
2015-11-19 14:22:54 -0500

    Worked on various stuff

--- AposSong.py
+++ AposSong.py
@@ -1,28 +1,21 @@
-import sys
+import sys, json
 from PyQt4 import QtGui, QtCore
 
 class ControlRoom(QtGui.QWidget):
     def __init__(self, songList):
         super(ControlRoom, self).__init__()
         self.setWindowTitle("ControlRoom")
-        self.text = 'Cool Dialog'
+        self.text = "Cool Dialog"
         self.toggleFullscreen = False;
         self.songList = songList
-        print (self.songList[0].title)
+        print (self.songList[0]["title"])
 
     def keyPressEvent(self, e):
         if e.key() == QtCore.Qt.Key_F:
             self.toggleFullscreen = not self.toggleFullscreen
             self.fullscreen(self.toggleFullscreen)
-        if e.key() == QtCore.Qt.Key_D:
-            if self.w is None:
-                self.w = ControlRoom(self.songList)
-                self.w.show()
-            else:
-                self.w.show()
 
     def fullscreen(self, state):
-
         if state:
             self.showFullScreen()
         else:
@@ -69,34 +62,40 @@ class ControlRoom(QtGui.QWidget):
         for i in self.songList:
 
             path = QtGui.QPainterPath()
-            path.addText(textX, textY, font, i.title)
+            path.addText(textX, textY, font, i["title"])
 
             qp.drawPath(path)
 
             textY += textHeight
-
-class SingleSong():
-    def __init__(self, title, parts):
-        self.title = title
-        #Songs are broken into parts.
-        ##Parts contains lists of lines.
-        self.parts = parts
 
 class AposSong(QtGui.QWidget):
     
     def __init__(self):
         super(AposSong, self).__init__()
         
-        self.toggleFullscreen = False;
-        self.text = "Hello World!"
+        self.widthMargin = 50
+        self.heightMargin = 50
+
+        self.selectedSong = 0
+        self.selectedPart = 0
+
+        self.toggleFullscreen = False
+        self.showTitle = False
 
         self.w = None
 
         #Dict that contains labels for song titles
         self.songList = []
-        self.songList.append(SingleSong("Hello World!", [['Line1', 'Line2'], ['Second part line1', 'Second part line2.']]))
-        self.songList.append(SingleSong("Hello World!", [['Line1', 'Line2'], ['Second part line1', 'Second part line2.']]))
-        self.songList.append(SingleSong("Praise!", [['Line1', 'Line2'], ['Second part line1', 'Second part line2.']]))
+
+        with open('chansons.json', 'r', encoding='utf8') as fp:
+            self.songList = json.load(fp)
+
+        #self.songList.append({"title": "Hello World!", "parts": [['Line1', 'Line2'], ['Second part line1', 'Second part line2.', 'YAY! Line3']]})
+        #self.songList.append({"title": "Hello World!", "parts": [['Line1', 'Line2'], ['Second part line1', 'Second part line2.']]})
+        #self.songList.append({"title": "Praise!", "parts": [['Line1', 'Line2'], ['Second part line1', 'Second part line2.']]})
+
+        #with open('chansons.json', 'w', encoding='utf8') as fp:
+        #    json.dump(self.songList, fp)
 
         self.initUI()
         
@@ -120,6 +119,15 @@ class AposSong(QtGui.QWidget):
                 self.w.show()
             else:
                 self.w.show()
+        if e.key() == QtCore.Qt.Key_Right:
+            print ("Next part")
+            self.selectedPart = (self.selectedPart + 1) % len(self.songList[self.selectedSong]["parts"])
+            self.update()
+        if e.key() == QtCore.Qt.Key_Left:
+            print ("Previous part")
+            self.selectedPart = (self.selectedPart - 1) % len(self.songList[self.selectedSong]["parts"])
+            self.update()
+
 
     def fullscreen(self, state):
 
@@ -128,29 +136,51 @@ class AposSong(QtGui.QWidget):
         else:
             self.showNormal()
 
-    def paintEvent(self, e):
-        #TODO: Center the text as a whole.
-        #      Also, resize the text to fit the screen.
-        qp = QtGui.QPainter()
-        qp.begin(self)
-        qp.setRenderHint(QtGui.QPainter.Antialiasing)
-        qp.setRenderHint(QtGui.QPainter.HighQualityAntialiasing)
-
-        brush = QtGui.QBrush(QtCore.Qt.SolidPattern)
-        qp.setBrush(brush)
-        qp.drawRect(0, 0, self.width(), self.height())
-
-        brush = QtGui.QBrush(QtGui.QColor(255, 255, 255, 25))
-        brush.setStyle(QtCore.Qt.BDiagPattern)
-        qp.setBrush(brush)
-        qp.drawRect(0, 0, self.width(), self.height())
-
-        centerX = self.width() / 2
-        centerY = self.height() / 2
-
-        font = QtGui.QFont('Source Code Pro Light', 100)
+    def getWidth(self, text, size):
+        font = QtGui.QFont('Source Code Pro Light', size)
         fm = QtGui.QFontMetrics(font)
-        textWidth = fm.width(self.text)
+        textWidth = fm.width(text)
+        textHeight = fm.height()
+
+        return textWidth, textHeight
+
+    def getMaxTextSize(self, text, maxWidth, maxHeight, lineCount):
+        size = 200
+        width, height = self.getWidth(text, size)
+
+        while (width > maxWidth or (height * lineCount) > maxHeight) and size > 1:
+            size -= 1
+            width, height = self.getWidth(text, size)
+        return size
+
+    def marginWidth(self):
+        return self.width() - self.widthMargin * 2
+
+    def marginHeight(self):
+        return self.height() - self.heightMargin * 2
+
+    def fitText(self, qp, title, textLines):
+        centerX = self.widthMargin + self.marginWidth() / 2
+        centerY = self.heightMargin + self.marginHeight() / 2
+
+        lineCount = len(textLines)
+        longestText = ""
+        longestTextLen = 0
+
+        if self.showTitle:
+            lineCount += 2
+            longestText = title
+            longestTextLen = len(title)
+
+        for i in textLines:
+            if len(i) > longestTextLen:
+                longestText = i
+                longestTextLen = len(i)
+
+        textSize = self.getMaxTextSize(longestText, self.marginWidth(), self.marginHeight(), lineCount)
+        font = QtGui.QFont('Source Code Pro Light', textSize)
+        fm = QtGui.QFontMetrics(font)
+        textWidth = fm.width(longestText)
         textHeight = fm.height()
 
         pen = QtGui.QPen()
@@ -158,24 +188,47 @@ class AposSong(QtGui.QWidget):
         pen.setColor(QtCore.Qt.gray)
         qp.setPen(pen)
 
-        selected = 0
-
         textX = centerX - (textWidth / 2)
-        textY = centerY - (textHeight * len(self.songList[selected].parts[1])) / 2
+        textY = centerY - (textHeight * len(textLines)) / 2
 
-        path = QtGui.QPainterPath()
-        path.addText(textX, textY, font, self.songList[selected].title)
+        if self.showTitle:
+            path = QtGui.QPainterPath()
+            path.addText(textX, textY, font, title)
 
-        qp.drawPath(path)
-        textY += textHeight
+            qp.drawPath(path)
+            textY += textHeight * 2
 
         qp.setBrush(QtGui.QBrush(QtGui.QColor(255, 255, 255, 200)))
-        for i in self.songList[0].parts[1]:
+        for i in textLines:
             path = QtGui.QPainterPath()
             path.addText(textX, textY, font, i)
 
             qp.drawPath(path)
             textY += textHeight
+
+    def drawBackground(self, qp):
+        qp.drawRect(0, 0, self.width(), self.height())
+
+        brush = QtGui.QBrush(QtGui.QColor(255, 255, 255, 25))
+        brush.setStyle(QtCore.Qt.BDiagPattern)
+        qp.setBrush(brush)
+        qp.drawRect(0, 0, self.width(), self.height())
+
+    def paintEvent(self, e):
+        #TODO: Center the text as a whole.
+        #      Also, resize the text to fit the screen.
+
+        qp = QtGui.QPainter()
+        qp.begin(self)
+        qp.setRenderHint(QtGui.QPainter.Antialiasing)
+        qp.setRenderHint(QtGui.QPainter.HighQualityAntialiasing)
+
+        brush = QtGui.QBrush(QtCore.Qt.SolidPattern)
+        qp.setBrush(brush)
+        
+        self.drawBackground(qp)
+
+        self.fitText(qp, self.songList[self.selectedSong]["title"], self.songList[self.selectedSong]["parts"][self.selectedPart])
 
 
         qp.end()
