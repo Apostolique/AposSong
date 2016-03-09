9c4a24750df5e45b3982f576a0a35b9d2f7bc33b
Apostolique <jeandavidmoisan@gmail.com>
2016-03-09 02:03:55 -0500

    Worked on various stuff
    Guess it's about time I push my changes.

--- AposSong.py
+++ AposSong.py
@@ -1,19 +1,137 @@
 import sys, json
 from PyQt4 import QtGui, QtCore
+import unicodedata
+
+class TextSearch:
+    #Removes accents and lowers the cases.
+    def normalizeText(self, text):
+        return ''.join((c for c in unicodedata.normalize('NFD', text) if unicodedata.category(c) != 'Mn')).lower()
+
+    def normalStringSearch(self, searchString, text):
+        return self.normalizeText(searchString) in self.normalizeText(text)
+
+    def normalPartSearch(self, searchString, parts):
+        for i in parts:
+            if self.normalStringSearch(searchString, " ".join(i)):
+                return True
+        return False
+
+    def normalSearch(self, searchString, song):
+        return self.normalStringSearch(searchString, song["title"]) or self.normalPartSearch(searchString, song["parts"])
+
+    def normalSearchAll(self, searchString, songList):
+        resultList = []
+        for i, s in enumerate(songList):
+            if self.normalSearch(searchString, s):
+                resultList.append(i)
+        return resultList
+
+class TextFitter:
+    def __init__(self):
+        pass
+
+    def getWidth(self, text, size):
+        font = QtGui.QFont('Source Code Pro Light', size)
+        fm = QtGui.QFontMetrics(font)
+        textWidth = fm.width(text)
+        textHeight = fm.height()
+
+        return textWidth, textHeight
+
+    def marginWidth(self, widthMargin, width):
+        return width - widthMargin * 2
+
+    def marginHeight(self, heightMargin, height):
+        return height - heightMargin * 2
+
+    def getMaxTextSize(self, text, maxWidth, maxHeight, lineCount):
+        size = 200
+        width, height = self.getWidth(text, size)
+
+        while (width > maxWidth or (height * lineCount) > maxHeight) and size > 1:
+            size -= 1
+            width, height = self.getWidth(text, size)
+        return size
+
+    #TODO: Split too long rows into columns.
+    def fitText(self, qp, title, textLines, widthMargin, heightMargin, width, height, showTitle, splitColumns):
+        centerX = widthMargin + self.marginWidth(widthMargin, width) / 2
+        centerY = heightMargin + self.marginHeight(heightMargin, height) / 2
+
+        lineCount = len(textLines)
+        longestText = ""
+        longestTextLen = 0
+
+        if showTitle:
+            lineCount += 2
+            longestText = title
+            longestTextLen = len(title)
+
+        for i in textLines:
+            if len(i) > longestTextLen:
+                longestText = i
+                longestTextLen = len(i)
+
+        textSize = self.getMaxTextSize(longestText, self.marginWidth(widthMargin, width), self.marginHeight(heightMargin, height), lineCount)
+        font = QtGui.QFont('Source Code Pro Light', textSize)
+        fm = QtGui.QFontMetrics(font)
+        textWidth = fm.width(longestText)
+        textHeight = fm.height()
+
+        pen = QtGui.QPen()
+        pen.setWidth(3)
+        pen.setColor(QtCore.Qt.gray)
+        qp.setPen(pen)
+
+        textX = centerX - (textWidth / 2)
+        textY = centerY - (textHeight * len(textLines)) / 2
+
+        if showTitle:
+            path = QtGui.QPainterPath()
+            path.addText(textX, textY, font, title)
+
+            qp.drawPath(path)
+            textY += textHeight * 2
+
+        qp.setBrush(QtGui.QBrush(QtGui.QColor(255, 255, 255, 200)))
+        for i in textLines:
+            path = QtGui.QPainterPath()
+            path.addText(textX, textY, font, i)
+
+            qp.drawPath(path)
+            textY += textHeight
+
+    def drawBackground(self, qp, width, height):
+        qp.drawRect(0, 0, width, height)
+
+        brush = QtGui.QBrush(QtGui.QColor(255, 255, 255, 25))
+        brush.setStyle(QtCore.Qt.BDiagPattern)
+        qp.setBrush(brush)
+        qp.drawRect(0, 0, width, height)
 
 class ControlRoom(QtGui.QWidget):
-    def __init__(self, songList):
+    def __init__(self, parent, songList, textFitter):
         super(ControlRoom, self).__init__()
+        self.parent = parent
+
+        self.textFitter = textFitter
         self.setWindowTitle("ControlRoom")
-        self.text = "Cool Dialog"
+        self.widthMargin = 50
+        self.heightMargin = 50
+
         self.toggleFullscreen = False;
         self.songList = songList
+        self.selectedSong = 0
         print (self.songList[0]["title"])
 
     def keyPressEvent(self, e):
-        if e.key() == QtCore.Qt.Key_F:
+        if not self.parent.searchMode and e.key() == QtCore.Qt.Key_F:
             self.toggleFullscreen = not self.toggleFullscreen
             self.fullscreen(self.toggleFullscreen)
+        else:
+            self.parent.keyPressEvent(e)
+    def setSelectedSong(self, number):
+        self.selectedSong = number
 
     def fullscreen(self, state):
         if state:
@@ -26,53 +144,43 @@ class ControlRoom(QtGui.QWidget):
         qp.setRenderHint(QtGui.QPainter.Antialiasing)
         qp.setRenderHint(QtGui.QPainter.HighQualityAntialiasing)
 
-        font = QtGui.QFont('Source Code Pro Light', 100)
-        fm = QtGui.QFontMetrics(font)
-        textWidth = fm.width(self.text)
-        textHeight = fm.height()
-
         brush = QtGui.QBrush(QtCore.Qt.SolidPattern)
         qp.setBrush(brush)
         qp.drawRect(0, 0, self.width(), self.height())
 
-        brush = QtGui.QBrush(QtGui.QColor(255, 255, 255, 25))
-        brush.setStyle(QtCore.Qt.BDiagPattern)
-        qp.setBrush(brush)
-        qp.drawRect(0, 0, self.width(), self.height())
+        self.textFitter.drawBackground(qp, self.width(), self.height())
 
+        #Draw the red corners of the control room.
         qp.setPen(QtGui.QPen(QtGui.QColor(255, 0, 0, 50), 10, QtCore.Qt.SolidLine))
         qp.drawLine(0, 100, 100, 0)
         qp.drawLine(self.width() - 100, 0, self.width(), 100)
         qp.drawLine(0, self.height() - 100, 100, self.height())
         qp.drawLine(self.width() - 100, self.height(), self.width(), self.height() - 100)
 
-        centerX = self.width() / 2
-        centerY = self.height() / 2
+        titleList = []
+        for i, s in enumerate(self.songList):
+            if i == self.selectedSong:
+                titleList.append("-" + s["title"])
+            else:
+                titleList.append(s["title"])
 
-        qp.setBrush(QtGui.QBrush(QtGui.QColor(255, 255, 255, 200)))
-        pen = QtGui.QPen()
-        pen.setWidth(3)
-        pen.setColor(QtCore.Qt.gray)
-        qp.setPen(pen)
+        self.textFitter.fitText(qp, self.songList[self.selectedSong]["title"], titleList, self.widthMargin, self.heightMargin, self.width(), self.height(), True, True)
 
+        #Write search string:
+        font = QtGui.QFont('Source Code Pro Light', 24)
 
-        textX = centerX - (textWidth / 2)
-        textY = centerY + textHeight - (textHeight * len(self.songList)) / 2
-
-        for i in self.songList:
-
-            path = QtGui.QPainterPath()
-            path.addText(textX, textY, font, i["title"])
-
-            qp.drawPath(path)
-
-            textY += textHeight
+        path = QtGui.QPainterPath()
+        path.addText(50, 50, font, self.parent.searchString)
+        qp.drawPath(path)
 
 class AposSong(QtGui.QWidget):
     
     def __init__(self):
         super(AposSong, self).__init__()
         
+        self.textFitter = TextFitter()
+        self.textSearch = TextSearch()
+
         self.widthMargin = 50
         self.heightMargin = 50
 
@@ -81,6 +189,9 @@ class AposSong(QtGui.QWidget):
 
         self.toggleFullscreen = False
         self.showTitle = False
+        self.searchMode = False
+
+        self.searchString = ""
 
         self.w = None
 
@@ -110,24 +221,113 @@ class AposSong(QtGui.QWidget):
         #    print(family)
 
     def keyPressEvent(self, e):
-        if e.key() == QtCore.Qt.Key_F:
-            self.toggleFullscreen = not self.toggleFullscreen
-            self.fullscreen(self.toggleFullscreen)
-        if e.key() == QtCore.Qt.Key_D:
-            if self.w is None:
-                self.w = ControlRoom(self.songList)
-                self.w.show()
-            else:
-                self.w.show()
-        if e.key() == QtCore.Qt.Key_Right:
-            print ("Next part")
-            self.selectedPart = (self.selectedPart + 1) % len(self.songList[self.selectedSong]["parts"])
-            self.update()
-        if e.key() == QtCore.Qt.Key_Left:
-            print ("Previous part")
-            self.selectedPart = (self.selectedPart - 1) % len(self.songList[self.selectedSong]["parts"])
-            self.update()
+        if self.searchMode:
+            if e.key() == QtCore.Qt.Key_Escape:
+                print ("Escaped to default mode.")
+                self.searchMode = False
+                self.searchString = ""
+                if self.w is not None:
+                    self.w.update()
+            elif e.key() == QtCore.Qt.Key_Enter or e.key() == QtCore.Qt.Key_Return:
+                print ("Searching now! {}".format(self.searchString))
+                resultList = self.textSearch.normalSearchAll(self.searchString, self.songList)
 
+                print (resultList)
+                for i in resultList:
+                    print (self.songList[i]["title"])
+
+                self.searchMode = False
+            elif e.key() == QtCore.Qt.Key_Backspace:
+                self.searchString = self.searchString[:-1]
+                print ("Remove last")
+                if self.w is not None:
+                    self.w.update()
+            else:
+                try:
+                    char = "%c" % (e.key())
+                    print ("Key pressed: {}, was: {}".format(char, e.key()))
+
+                    self.searchString += char.lower()
+                    if self.w is not None:
+                        self.w.update()
+
+                except OverflowError:
+                    print ("Key pressed: error, was: {}".format(e.key()))
+                    pass
+        else:
+            if e.key() == QtCore.Qt.Key_F:
+                self.toggleFullscreen = not self.toggleFullscreen
+                self.fullscreen(self.toggleFullscreen)
+            elif e.key() == QtCore.Qt.Key_D:
+                if self.w is None:
+                    self.w = ControlRoom(self, self.songList, self.textFitter)
+                    self.w.show()
+                else:
+                    self.w.show()
+            elif e.key() == QtCore.Qt.Key_Right:
+                print ("Next part")
+                self.selectedPart = (self.selectedPart + 1) % len(self.songList[self.selectedSong]["parts"])
+                self.update()
+            elif e.key() == QtCore.Qt.Key_Left:
+                print ("Previous part")
+                self.selectedPart = (self.selectedPart - 1) % len(self.songList[self.selectedSong]["parts"])
+                self.update()
+            elif e.key() == QtCore.Qt.Key_Up:
+                print ("Previous Song")
+                self.selectedPart = 0
+                self.selectedSong = (self.selectedSong - 1) % len(self.songList)
+                self.update()
+                if self.w is not None:
+                    self.w.setSelectedSong(self.selectedSong)
+                    self.w.update()
+            elif e.key() == QtCore.Qt.Key_Down:
+                print ("Next Song")
+                self.selectedPart = 0
+                self.selectedSong = (self.selectedSong + 1) % len(self.songList)
+                self.update()
+                if self.w is not None:
+                    self.w.setSelectedSong(self.selectedSong)
+                    self.w.update()
+            elif e.key() == QtCore.Qt.Key_1:
+                self.selectedPart = 1 % len(self.songList[self.selectedSong]["parts"])
+                self.update()
+            elif e.key() == QtCore.Qt.Key_2:
+                self.selectedPart = 2 % len(self.songList[self.selectedSong]["parts"])
+                self.update()
+            elif e.key() == QtCore.Qt.Key_3:
+                self.selectedPart = 3 % len(self.songList[self.selectedSong]["parts"])
+                self.update()
+            elif e.key() == QtCore.Qt.Key_4:
+                self.selectedPart = 4 % len(self.songList[self.selectedSong]["parts"])
+                self.update()
+            elif e.key() == QtCore.Qt.Key_5:
+                self.selectedPart = 5 % len(self.songList[self.selectedSong]["parts"])
+                self.update()
+            elif e.key() == QtCore.Qt.Key_6:
+                self.selectedPart = 6 % len(self.songList[self.selectedSong]["parts"])
+                self.update()
+            elif e.key() == QtCore.Qt.Key_7:
+                self.selectedPart = 7 % len(self.songList[self.selectedSong]["parts"])
+                self.update()
+            elif e.key() == QtCore.Qt.Key_8:
+                self.selectedPart = 8 % len(self.songList[self.selectedSong]["parts"])
+                self.update()
+            elif e.key() == QtCore.Qt.Key_9:
+                self.selectedPart = 9 % len(self.songList[self.selectedSong]["parts"])
+                self.update()
+            elif e.key() == QtCore.Qt.Key_0:
+                self.selectedPart = 0 % len(self.songList[self.selectedSong]["parts"])
+                self.update()
+            elif e.key() == QtCore.Qt.Key_S:
+                self.searchMode = True
+                self.searchString = ""
+                print ("SearchMode: {}".format(self.searchMode))
+                if self.w is not None:
+                    self.w.update()
+            elif e.key() == QtCore.Qt.Key_T:
+                self.showTitle = not self.showTitle
+                self.update()
+                print ("Toggle Title!")
 
     def fullscreen(self, state):
 
@@ -135,84 +335,6 @@ class AposSong(QtGui.QWidget):
             self.showFullScreen()
         else:
             self.showNormal()
-
-    def getWidth(self, text, size):
-        font = QtGui.QFont('Source Code Pro Light', size)
-        fm = QtGui.QFontMetrics(font)
-        textWidth = fm.width(text)
-        textHeight = fm.height()
-
-        return textWidth, textHeight
-
-    def getMaxTextSize(self, text, maxWidth, maxHeight, lineCount):
-        size = 200
-        width, height = self.getWidth(text, size)
-
-        while (width > maxWidth or (height * lineCount) > maxHeight) and size > 1:
-            size -= 1
-            width, height = self.getWidth(text, size)
-        return size
-
-    def marginWidth(self):
-        return self.width() - self.widthMargin * 2
-
-    def marginHeight(self):
-        return self.height() - self.heightMargin * 2
-
-    def fitText(self, qp, title, textLines):
-        centerX = self.widthMargin + self.marginWidth() / 2
-        centerY = self.heightMargin + self.marginHeight() / 2
-
-        lineCount = len(textLines)
-        longestText = ""
-        longestTextLen = 0
-
-        if self.showTitle:
-            lineCount += 2
-            longestText = title
-            longestTextLen = len(title)
-
-        for i in textLines:
-            if len(i) > longestTextLen:
-                longestText = i
-                longestTextLen = len(i)
-
-        textSize = self.getMaxTextSize(longestText, self.marginWidth(), self.marginHeight(), lineCount)
-        font = QtGui.QFont('Source Code Pro Light', textSize)
-        fm = QtGui.QFontMetrics(font)
-        textWidth = fm.width(longestText)
-        textHeight = fm.height()
-
-        pen = QtGui.QPen()
-        pen.setWidth(3)
-        pen.setColor(QtCore.Qt.gray)
-        qp.setPen(pen)
-
-        textX = centerX - (textWidth / 2)
-        textY = centerY - (textHeight * len(textLines)) / 2
-
-        if self.showTitle:
-            path = QtGui.QPainterPath()
-            path.addText(textX, textY, font, title)
-
-            qp.drawPath(path)
-            textY += textHeight * 2
-
-        qp.setBrush(QtGui.QBrush(QtGui.QColor(255, 255, 255, 200)))
-        for i in textLines:
-            path = QtGui.QPainterPath()
-            path.addText(textX, textY, font, i)
-
-            qp.drawPath(path)
-            textY += textHeight
-
-    def drawBackground(self, qp):
-        qp.drawRect(0, 0, self.width(), self.height())
-
-        brush = QtGui.QBrush(QtGui.QColor(255, 255, 255, 25))
-        brush.setStyle(QtCore.Qt.BDiagPattern)
-        qp.setBrush(brush)
-        qp.drawRect(0, 0, self.width(), self.height())
 
     def paintEvent(self, e):
         #TODO: Center the text as a whole.
@@ -226,9 +348,9 @@ class AposSong(QtGui.QWidget):
         brush = QtGui.QBrush(QtCore.Qt.SolidPattern)
         qp.setBrush(brush)
         
-        self.drawBackground(qp)
+        self.textFitter.drawBackground(qp, self.width(), self.height())
 
-        self.fitText(qp, self.songList[self.selectedSong]["title"], self.songList[self.selectedSong]["parts"][self.selectedPart])
+        self.textFitter.fitText(qp, self.songList[self.selectedSong]["title"], self.songList[self.selectedSong]["parts"][self.selectedPart], self.widthMargin, self.heightMargin, self.width(), self.height(), self.showTitle, False)
 
 
         qp.end()
